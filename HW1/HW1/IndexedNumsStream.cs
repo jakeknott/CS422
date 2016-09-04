995e2f138a412a7665218b39037516ac6e710f32
Jake Knott <knott7030@gmail.com>
2016-09-04 15:35:34 -0700

    HW1 final

---
+++ HW1/HW1/IndexedNumsStream.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+
+namespace CS422
+{
+	public class IndexedNumsStream : System.IO.Stream
+	{
+		private long _length;
+		private long _position;
+
+		public IndexedNumsStream (long length)
+		{
+			SetLength (length);
+			_length = Length;
+			_position = 0;
+		}
+
+		public override bool CanRead {
+			get { return true; }
+		}
+
+		public override bool CanSeek {
+			get { return true; }
+		}
+
+		public override bool CanWrite {
+			get { return false;}
+		}
+
+		public override long Length {
+			get { return _length; }
+		}
+
+		public override long Position {
+			get { return _position; }
+
+			set 
+			{
+				_position = value;
+				if (_position < 0) 
+				{
+					_position = 0;
+				} 
+				else if (_position >= _length) 
+				{
+					_position = _length;
+				}
+			}
+		}
+
+		public override void Flush ()
+		{
+		}
+
+		public override long Seek (long offset, System.IO.SeekOrigin origin)
+		{
+			if (origin.Equals (SeekOrigin.End)) 
+			{
+				Position = _length;
+			} 
+			else 
+			{
+				Position = offset;
+			}
+	
+			return Position;
+		}
+
+		public override void SetLength (long value)
+		{
+			if (value < 0) 
+			{
+				_length = 0;
+			}
+			else 
+			{
+				_length = value - 1;
+			}
+				
+		}
+
+		public override int Read (byte[] buffer, int offset, int count)
+		{
+			int bytesWrote = 0;
+
+			for (int i = 0; i < count; i++) 
+			{
+				if ((i + offset) < buffer.Length)
+				{
+					buffer [i + offset] = (byte)(Position % 256);
+					Position++;
+					bytesWrote++;
+				}
+			}
+
+			return bytesWrote;
+		}
+
+		public override void Write (byte[] buffer, int offset, int count)
+		{
+		}
+	}
+}
+
---
+++ HW1/HW1/NumberTextWriter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CS422
+{
+	public class NumberedTextWriter : System.IO.TextWriter
+	{
+		private int _lineNumber;
+		private TextWriter _writer; 
+
+		public NumberedTextWriter(TextWriter wrapThis)
+		{
+			_lineNumber = 1;
+			_writer = wrapThis;
+		}
+
+		public NumberedTextWriter(TextWriter wrapThis, int startingLineNumber)
+		{
+			_lineNumber = startingLineNumber;
+			_writer = wrapThis;
+		}
+
+		public override void WriteLine (string value)
+		{
+			string newValue = _lineNumber + ": " + value;
+
+			_writer.WriteLine (newValue);
+
+			_lineNumber++;
+		}
+
+		public override System.Text.Encoding Encoding 
+		{
+			get { return _writer.Encoding; }
+		}
+
+	}
+}
+
