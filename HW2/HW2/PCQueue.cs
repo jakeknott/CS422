46f70fd7cf37bb3290023b3c338368f444c6a71a
Jake Knott <knott7030@gmail.com>
2016-09-08 13:06:16 -0700

    First sub

--- HW2/HW2/PCQueue.cs
+++ HW2/HW2/PCQueue.cs
@@ -6,64 +6,62 @@ namespace CS422
 	{
 		public int value; 
 		public Node next;
+
+		public Node ()
+		{
+			value = 0;
+			next = null;
+		}
+
+		public Node (int val)
+		{
+			value = val;
+			next = null;
+		}
 	}
 
 	public class PCQueue
 	{
-		private Node _dummyNode = new Node();
-		Node _statNode = new Node();
-		Node _endNode = new Node();
+		private Node _dummyNode;
+		Node _startNode;
+		Node _endNode;
 
 		public PCQueue ()
 		{
-			_statNode = _dummyNode;
+			_dummyNode = new Node ();
+			_startNode = _dummyNode;
 			_endNode = _dummyNode;
 		}
 
 		public void Enqueue(int dataValue)
 		{
-			_endNode.next = newNode (dataValue);
+			// Since the start is always a dummy, we do not need to check if it is null.
+			// Just make a new node and add it to the end of the list.
+			_endNode.next = new Node (dataValue);
 			_endNode = _endNode.next;
 		}
 
 		public bool Dequeue(ref int out_value)
 		{
-			// front is never null since it will be a dummy node
-			//if (object.ReferenceEquals(_statNode, _dummyNode))
-			//{
-				
-				//if (front has a next (not null))
-				//{
-				//	Move front ahead
-				//	Front = front.next;
-				// decide if the old front is the back, then we need to set it to the 	dummy
-				//}
-
-			//}/
-
-			if (!object.ReferenceEquals(_statNode, _endNode))
+			// Since the start is always a dummy, check if there is another node after
+			if (_startNode.next != null)
 			{
-				//
-			}
-			else 
-			{
-				
-				// more than one item
-				//Object o = front.data;
-				//front = front.next;
-				//Out_val = o;
-				//Return true;
+				// If there is another node, then move the front node forward one and 
+				// set the out value to the start's next (which is the first item in the list).
+				out_value = _startNode.next.value;
+
+				// start's next's next is either a node (the case where there are multiple nodes in the list)
+				// Or start's next's next is null (based on node constructer) (the case where there was only the one 
+				// node in the list. 
+				_startNode.next = _startNode.next.next;
+
+				return true;
 			}
 
+			// If there is only the start node, no node after, then return false
+			// since the start node is always the dummy node. 
 			return false;
 		}
 
-		private Node newNode (int val)
-		{
-			Node returnNode = new Node ();
-			returnNode.value = val;
-			return returnNode;
-		}
 	}
 }
-
---
+++ HW2/HW2/ThreadPoolSleepSorter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+
+namespace CS422
+{
+	public class ThreadPoolSleepSorter : IDisposable
+	{
+		private TextWriter _writter;
+		private BlockingCollection<ThreadPoolSleepSorter> coll;
+		private byte _sleepTime;
+		private List<Thread> allThreads;
+
+		private ThreadPoolSleepSorter()
+		{}
+
+		private ThreadPoolSleepSorter(byte sleepTime, TextWriter output)
+		{
+			_sleepTime = sleepTime;
+			_writter = output;
+		}
+
+		public ThreadPoolSleepSorter(TextWriter output, ushort threadCount)
+		{
+			allThreads = new List<Thread>{ };
+
+			coll = new BlockingCollection<ThreadPoolSleepSorter>();
+			_writter = output;
+
+			if (threadCount == 0)
+			{
+				threadCount = 64;
+			}
+
+			for (int i = 0; i < threadCount; i++)
+			{
+				ThreadPoolSleepSorter sleeper = new ThreadPoolSleepSorter ();
+				Thread t = new Thread (sleeper.ThreadWorkFunc);
+				t.Start (coll);
+				allThreads.Add (t);
+			}
+		}
+
+		public void Sort(byte[] values)
+		{
+			foreach (var item in values)
+			{
+				ThreadPoolSleepSorter sleeper = new ThreadPoolSleepSorter (item, _writter);
+				coll.Add (sleeper);
+			}
+		}
+
+		// ThreadWorkFunc logic:
+		public void ThreadWorkFunc(object blockColl) 
+		{
+			BlockingCollection<ThreadPoolSleepSorter> collection = (BlockingCollection<ThreadPoolSleepSorter>)blockColl;
+			while (true)
+			{				
+				collection.Take ().Sleep ();
+			}
+		}
+
+		public void Sleep()
+		{
+			Thread.Sleep (_sleepTime * 1000);
+			_writter.WriteLine (_sleepTime);
+		}
+
+		public void Dispose ()
+		{
+			foreach (var thread in allThreads)
+			{
+				thread.Abort ();
+			}
+		}
+	}
+}
