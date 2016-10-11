ee1b55167609f1704ebce295acbf70ad92a7e891
knott7030@gmail.com <knott7030@gmail.com>
2016-10-11 14:38:50 -0700

    Homework 7 done, adding timeouts to the server core

---
+++ HW7/HW7/DemoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CS422
+{
+	internal class DemoService : WebService
+	{
+		private const string c_template =
+			"<html>This is the response to the request:<br>" +
+			"Method: {0}<br>Request-Target/URI: {1}<br>" +
+			"Request body size, in bytes: {2}<br><br>" +
+			"Student ID: {3}</html>";
+
+		public DemoService ()
+		{
+		}
+
+		public override void Handler (WebRequest req)
+		{
+			string bodyLength;
+			try
+			{
+				// If the req had a content length header,
+				// this will work
+				bodyLength = req.BodyLength.ToString ();
+			}
+			catch
+			{
+				// otherwise, it will throw an exception because
+				// we do not know the length. 
+				bodyLength = "Undefined";
+			}
+
+			req.WriteHTMLResponse (string.Format (c_template, req.Method, req.URI, bodyLength, "11398813"));
+		}
+
+		public override string ServiceURI {
+			get
+			{
+				return "/";
+			}
+		}
+	}
+}
+
---
+++ HW7/HW7/TestingFiles.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using System.Text;
+
+namespace CS422
+{
+	[TestFixture ()]
+	public class TestingFiles
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			string requestLine = "GET ";   
+
+			for (int i = 0; i < 2048; i++)
+			{
+				requestLine += "a";
+			}
+
+			requestLine += "HTTP/1.1\r\n\r\n";
+
+			byte[] byteArray = Encoding.UTF8.GetBytes(requestLine);
+			//byte[] byteArray = Encoding.ASCII.GetBytes(contents);
+			MemoryStream stream = new MemoryStream(byteArray);
+			ValidateRequest validate = new ValidateRequest ();
+
+			Assert.IsFalse (validate.IsValid (stream));
+
+			requestLine = "GET URL HTTP/1.1\r\n";   // this is valid
+
+			for (int i = 0; i < 100 * 1024; i++)
+			{
+				requestLine += "a:"; //adding a lot of giberish for the 'headers'
+			}
+
+			byteArray = Encoding.UTF8.GetBytes(requestLine);
+			stream = new MemoryStream(byteArray);
+			validate = new ValidateRequest ();
+			Assert.IsFalse (validate.IsValid (stream));
+
+
+			
+			WebServer server = new WebServer ();
+			WebServer.Start (4422, 0);
+			WebService service = new DemoService ();
+
+			server.AddService (service);
+
+			while(true)
+			{}
+
+
+			WebServer.Stop ();
+		}
+	}
+}
+
